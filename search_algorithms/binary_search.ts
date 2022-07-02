e844dff67199e9473205ed3fa16c53597e3e0758
Arthur Rogers <a23ryndin@gmail.com>
2022-07-02 15:05:10 +0300

    added typescript solution for binary search

---
+++ search_algorithms/binary_search.ts
@@ -0,0 +1,16 @@
+function binarySearch<Type>(arr: Array<Type>, val: Type): number {
+  let begin = 0;
+  let end = arr.length - 1;
+  let middile = Math.floor((begin + end) / 2);
+  while (arr[middile] !== val && begin < end) {
+    if (val < arr[middile]) end = middile - 1;
+    if (val > arr[middile]) begin = middile + 1;
+    middile = Math.floor((begin + end) / 2);
+  }
+
+  return arr[middile] === val ? middile : -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 6));
+console.log(binarySearch([1.22, 3.15, 4.22, 5.57, 8.99, 10.22], 4.22));
+console.log(binarySearch([1.22, 3.15, 4.22, 5.57, 8.99, 10.22], 0.02));
